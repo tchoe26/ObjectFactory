70b3e6d54c1bfe6658a920ac599ca7c65f6d3fd1
Teddy <theodore_choe26@milton.edu>
2023-11-02 09:36:53 -0400

    made constructor

---
+++ src/Ponies.java
@@ -0,0 +1,23 @@
+public class Ponies {
+
+   public boolean isUnicorn = false;
+   public   boolean isPegasus = false;
+    double size = 28.56;
+
+    String name = "Bobby";
+    String color = "Pink";
+
+    public void describePony() {
+        System.out.println("this is pony" + name + ", and they are" + color + ". The size is " + size + ".");
+    }
+
+    public Ponies(String pName, String pColor, Double pSize, Boolean pUnicorn, Boolean pPegasus) {
+        System.out.println("hi");
+        isUnicorn = false;
+        isPegasus = false;
+        size = 66.66;
+        name = "Bobby";
+
+    }
+
+}
