b7cc865b9d6f019ee7e2b5375eb65957c6446f51
tchoe26 <theodore_choe26@milton.edu>
2023-11-08 09:08:06 -0500

    11.8.2023
    made class tennisplayer, worked on calculator project

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,45 +1,56 @@
 import java.util.Objects;
 import java.util.Scanner;
 public class Calculator {
-
+    Scanner input = new Scanner(System.in);
+    int parameterCounter = 1;
+    double temporaryOutput;
+    double[] calculations = new double [1000];
+    String[] operators = new String [1000];
+    String temp = "hi";
     public Calculator() {
-        Scanner input = new Scanner(System.in);
-        int parameterCounter = 0;
+
+        System.out.println("calculator follows chronological order of operations");
+        inputCollecting();
+        calculating();
+    }
+    public void calculating() {
         if (Objects.equals(input.next(), "enter")){
 
+            if(Objects.equals(operators[1], "+"))
+                temporaryOutput = calculations[1]+calculations[2];
+            if(Objects.equals(operators[1], "-"))
+                temporaryOutput = calculations[1]-calculations[2];
+            if(Objects.equals(operators[1], "*"))
+                temporaryOutput = calculations[1]*calculations[2];
+            if(Objects.equals(operators[1], "/"))
+                temporaryOutput = calculations[1]/calculations[2];
 
+            for(int x=2; x<=parameterCounter; x++){
+                if(Objects.equals(operators[x],"+"))
+                    temporaryOutput = temporaryOutput+calculations[x+1];
+                if(Objects.equals(operators[x],"-"))
+                    temporaryOutput = temporaryOutput-calculations[x+1];
+                if(Objects.equals(operators[x],"*"))
+                    temporaryOutput = temporaryOutput*calculations[x+1];
+                if(Objects.equals(operators[x],"/"))
+                    temporaryOutput = temporaryOutput/calculations[x+1];
+            }
+            System.out.println(temporaryOutput);
         } else {
-            parameterCounter++;
-            System.out.println("Enter number " + parameterCounter);
-            double[] calculations = new double [1000];
-            
+           inputCollecting();
         }
-
-        System.out.println("Enter first number: ");
-        double firstCalculation = input.nextDouble();
-        System.out.println("enter second number:");
-        double secondCalculation = input.nextDouble();
-        System.out.println("Enter desired operation:");
-        String operation = input.next();
-        
-        double output = 0;
-        if (Objects.equals(operation, "+")) {
-            output = firstCalculation + secondCalculation;
-        }
-        if (Objects.equals(operation, "-")) {
-            output = firstCalculation - secondCalculation;
-        }
-        if (Objects.equals(operation, "*")) {
-            output = firstCalculation * secondCalculation;
-        }
-        if (Objects.equals(operation, "/")) {
-            output = firstCalculation / secondCalculation;
-        }
-
-        System.out.println(output);
-
     }
 
+    public void inputCollecting() {
+        System.out.println("Enter input " + parameterCounter);
 
-
+        calculations[parameterCounter] = Double.parseDouble(input.next());
+        System.out.println("enter operation or say 'enter' if done:");
+        temp = input.next();
+        if(Objects.equals(temp, "+")||Objects.equals(temp, "-")||Objects.equals(temp, "*")||Objects.equals(temp, "/"))
+            operators[parameterCounter] = input.next();
+        else { System.out.println("invalid operator; please use +,-,*,/"); }
+        parameterCounter++;
+        calculating();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        TennisPlayer abc;
+        abc = new TennisPlayer(47, 140, true, 1, "Nick Kyrgios", 1999);
+        abc.describeTennisPlayer();
 
         Ponies p = new Ponies("bobby", "yellow", 66.66, false, true);
 
@@ -11,6 +14,9 @@ public class Main {
 
         Ponies lol;
         lol = new Ponies("Eli", "blue", 42.0, false, true);
+        Ponies hehe;
+        hehe = new Ponies("hehe", "blue", 123.4, false, true);
+        hehe.describePony();
 
         lol.describePony();
 
